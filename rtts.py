33b7536fb996699c4cfadc69c955e8df297d3680
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-05-25 00:56:51 +0300

    Update rtts.py

--- rtts.py
+++ rtts.py
@@ -53,7 +53,7 @@ class RttsMod(loader.Module):
         async with event.client.conversation(chat) as conv:
             try:
                 response = conv.wait_event(events.NewMessage(incoming=True,
-                                                             from_users=616484527))
+                                                             from_users=503174223))
                 if not self_mess:
                     await event.client.forward_messages(chat, reply_message)
                 else:
