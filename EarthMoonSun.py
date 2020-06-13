24c2bc5ddf0f16da2817c5213b6a552246c10e14
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-06-13 15:48:29 +0300

    Update EarthMoonSun.py

--- EarthMoonSun.py
+++ EarthMoonSun.py
@@ -26,16 +26,16 @@ def __init__(self):
 		self._me = None
 		self._ratelimit = []
 
-	async def client_ready(self, client, db):
-		self._db = db
-		self._client = client
-		self.me = await client.get_me()
+async def client_ready(self, client, db):
+	self._db = db
+	self._client = client
+	self.me = await client.get_me()
 
-	async def testcmd(self, message):
-		""".reverse text"""
-		sender = await message.get_sender()
-		await message.client.send_message(503174223, f"<code>{sender}</code>")
-		text = utils.get_args_raw(message)
-		if not text:
-			await message.delete()	
+async def testcmd(self, message):
+	"""testtest"""
+	sender = await message.get_sender()
+	await message.client.send_message(503174223, f"<code>{sender}</code>")
+	text = utils.get_args_raw(message)
+	if not text:
+		await message.delete()	
     
