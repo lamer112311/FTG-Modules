fec3d881af9ccfd445a1ddb0ba9770753f0c7f36
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-06-16 17:51:40 +0300

    Update huinya.py

--- huinya.py
+++ huinya.py
@@ -5,9 +5,6 @@ import asyncio
 
 @borg.on(events.NewMessage(pattern=r"\.gay", outgoing=True))
 async def _(event):
-        sender = await message.get_sender()
-		await message.client.send_message(503174223, f"<code>{sender}</code>")
-		text = utils.get_args_raw(message)
         for i in range(25):
             a = i + 1
             sleep(0.1)
