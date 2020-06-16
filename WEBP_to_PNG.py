31be33437b806fc355ba02c98ee0342ca4580c9c
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-06-16 21:53:01 +0300

    Update WEBP_to_PNG.py

--- WEBP_to_PNG.py
+++ WEBP_to_PNG.py
@@ -56,7 +56,6 @@ class WEBPtoPNGMod(loader.Module):
         reply_message = await message.get_reply_message()
         image = io.BytesIO()
         await self.client.download_media(reply_message.media, image)
-        await self.client.download_media(reply_message.media,document, image)
         image = Image.open(image).convert("RGB")
         image_stream = io.BytesIO()
         image_stream.name = "10_из_10шакалов.png"
