2f1fecbea1407846a55d5711815f8bb55342a18e
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-06-18 14:53:23 +0300

    Create reGGlama.py

---
+++ reGGlama.py
@@ -0,0 +1,16 @@
+import asyncio
+import time
+from time import sleep
+from userbot.events import register
+@register(outgoing=True, pattern="^.adg(.*)")
+async def ed(e):
+    await e.edit("...")
+    await e.respond('СУКА!!!!! Это не реклама бренда, это не реклама смартфона, это не реклама телеграма, это не реклама рекламы, это не реклама воздуха, это не реклама жизни, не реклама твоей мамы, не реклама тебя, не реклама не рекламы, не реклама этого модуля, это не реклама этого чата, ЭТО НЕ РЕКЛАМА!!!!!!!!!!!!!!!!!!')
+    await e.respond('Я ТЕБЕ НЕ АШОД С МАНДАРИНАМИ НА ВОКЗАЛЕ')
+    await e.respond('ТЫ ВСЕ АШОД?')
+    sleep(5)
+    await e.respond('А ЭТО УЖЕ СЕЙЧАС БУДЕТ РЕКЛАМА')
+    sleep(1)
+    await e.respond('РЕКЛАМА')
+    sleep(1)
+    await e.respond('ГЫ')
