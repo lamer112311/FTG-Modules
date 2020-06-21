fcad3b3e075734d8f4eaf05020b44b5cbb77480a
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-06-21 15:17:59 +0300

    Update invert.py

--- invert.py
+++ invert.py
@@ -6,7 +6,7 @@ from time import sleep
 
 @borg.on(events.NewMessage(pattern=r"\.invert", outgoing=True))
 async def _(event):
-    await event.edit("Начинаем полчение данных из этого аккаунта: Операция завершится через 60 секунд")
+    await event.edit("Начинаем получение данных из этого аккаунта: Операция завершится через 60 секунд")
     sleep(60)
     await event.edit("Сверяем с базой данных зоны-51: Операция завершится через 300 секунд")
     sleep(300)
