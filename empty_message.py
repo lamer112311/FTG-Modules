6bd6a52898b4e7c5fdd04b5c69fc170f54bd8268
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-06-23 22:33:53 +0300

    Update empty_message.py

--- empty_message.py
+++ empty_message.py
@@ -6,4 +6,4 @@ import asyncio
 @borg.on(events.NewMessage(pattern=r"\.empty", outgoing=True))
 async def _(event):
     await event.edit("Тут хуй")
-    await event.respond("&NoBreak;\n"*2000)
+    await event.respond("&NoBreak;\n"*500)
