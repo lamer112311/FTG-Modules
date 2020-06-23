46e45a172aa534307e5953e1d9f6a6842b208269
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-06-23 22:28:22 +0300

    Update empty_message.py

--- empty_message.py
+++ empty_message.py
@@ -6,6 +6,4 @@ from time import sleep
 
 @borg.on(events.NewMessage(pattern=r"\.empty", outgoing=True))
 async def _(event):
-    await event.delete()
-    sleep(0.1)
     await event.respond("&NoBreak;\n"*2000)
