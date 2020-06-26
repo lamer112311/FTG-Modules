bbba4642aafe90f8759d259f62576e7ee7247ab0
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-06-26 16:09:45 +0300

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,27 +1,40 @@
-#by @laciamemeframe
-from .. import loader, utils
+from .. import loader, utils  # pylint: disable=relative-beyond-top-level
 import logging
 import requests
 
 logger = logging.getLogger(__name__)
 
+
 def register(cb):
-	cb(wMod())
+    cb(WWTrMod())
+
 
 @loader.tds
-class wMod(loader.Module):
-	"""Погода by wttr.in"""
-	strings = {'name': 'Погода'}
+class WWWTrMod(loader.Module):
+    """wttr.in"""
 
-	async def client_ready(self, client, db):
-		self.client = client
+    strings = {"name": "wttr"}
 
-	async def wcmd(self, message):
-		""".w <город> либо просто .w посмотреть погоду в локации где работает юзербот"""
-		message.edit("<b>Погода by wttr.in</b>")
-		city = utils.get_args(message)
-		await message.edit("Отправка данных на сервер wttr.in...")
-		r = requests.get("https://wttr.in/" + str(city) + "?format=%l:+%c+%t,+%w+%m")
-		await message.edit(r.text)
+    async def client_ready(self, client, db):
+        self.client = client
+
+    @loader.sudo
+    async def wcmd(self, message):
+        """.w <город>"""
+        message.edit("<b>Погода by wttr.in</b>")
+        city = utils.get_args(message)
+        msg = []
+        if city:
+            await message.edit("Обрабатываем запрос...")
+            for i in city:
+                r = requests.get(
+                    "https://wttr.in/" + i + "?format=%l:+%c+%t,+%w+%m"
+                )
+                msg.append(r.text)
+            await message.edit("".join(msg))
+        else:
+            await message.edit("Обрабатываем запрос...")
+            r = requests.get("https://wttr.in/?format=%l:+%c+%t,+%w+%m")
+            await message.edit(r.text)
 
 
