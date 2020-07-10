7ab784ddebfaa1e90d5224f3d26836b86faf1d11
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-07-10 14:03:34 +0300

    Update AudioShakal.py

--- AudioShakal.py
+++ AudioShakal.py
@@ -17,7 +17,7 @@ class AudioShakalMod(loader.Module):
 		self.me = await client.get_me()
 	async def fvcmd(self, message):
 		""".fv <reply to voice> [шакал_lvl(не обязательно, по умолчанию 100 (от 10 до 100))]
-		    Сшакалить войс
+		    Сшакалить войс
 		"""
 		reply = await message.get_reply_message()
 		lvl = 0
@@ -39,19 +39,29 @@ class AudioShakalMod(loader.Module):
 		else:
 			lvl = 100
 		await message.edit("<b>Шакалим...</b>")
+		sa = False
+		m = io.BytesIO()
 		fname = await message.client.download_media(message=reply.media)
-		if(fname.endswith(".oga")):
+		if(fname.endswith(".oga") or fname.endswith(".ogg")):
 			audio = AudioSegment.from_file(fname, "ogg")
+		elif(fname.endswith(".mp3")):
+			sa = True
+			audio = AudioSegment.from_file(fname, "mp3")
 		else:
 			await message.edit("<b>Я(.fv) не поддерживаю аудио файлы! Только голосовые!</b>")
 			os.remove(fname)
 			return
 		audio = audio + lvl
-		m = io.BytesIO()
-		m.name="voice.ogg"
-		audio.export(m, format="ogg")
-		await message.client.send_file(message.to_id, m, reply_to=reply.id, voice_note=True)
+		if(sa):
+			m.name = "Шакал.mp3"
+			audio.export(m, format="mp3")
+		else:
+			m.name="voice.ogg"
+			audio.export(m, format="ogg")
+		m.seek(0)
+		if(sa):
+			await message.client.send_file(message.to_id, m, reply_to=reply.id)
+		else:
+			await message.client.send_file(message.to_id, m, reply_to=reply.id, voice_note=True)
 		await message.delete()
 		os.remove(fname)
-
-		
