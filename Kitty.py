b5704057fd816a01c88339c52bdfbb37e6c099cc
LaciaMemeFrame <dima.skolpin@icloud.com>
2020-09-14 15:51:48 +0300

    Update Kitty.py

--- Kitty.py
+++ Kitty.py
@@ -40,7 +40,7 @@ class AnimalsMod(loader.Module):
              try:
                  response = conv.wait_event(events.NewMessage(incoming=True,
                                                               from_users=787358569))
-                 await event.client.send_message(chat, '🐈kitty')
+                 await event.client.send_message(chat, '/kitty')
                  response = await response
              except YouBlockedUserError:
                  await event.reply('<code>Разблокируй @pixelsetup_bot</code>')
@@ -64,7 +64,7 @@ class AnimalsMod(loader.Module):
              try:
                  response = conv.wait_event(events.NewMessage(incoming=True,
                                                               from_users=787358569))
-                 await event.client.send_message(chat, '🦮dogs')
+                 await event.client.send_message(chat, '/dogs')
                  response = await response
              except YouBlockedUserError:
                  await event.reply('<code>Разблокируй @pixelsetup_bot</code>')
